0845966aa69ed62b7eb9c003397af941ca2ada01
Adhithiyan Azhagudurai <adhi.cdm@gmail.com>
2023-02-13 11:40:58 +0530

    New additions

--- main.py
+++ main.py
@@ -5,10 +5,18 @@ app = Flask(__name__)
 def home():
    return render_template('home.html')
 
+@app.route('/gcp')
+def gcp():
+   return render_template('gcp.html')   
+
 @app.route('/opengles3')
 def opengles3():
    return render_template('opengles3.html')
 
+
+@app.route('/questions')
+def questions():
+   return render_template('questions.html')
 
 
 if __name__ == '__main__':
